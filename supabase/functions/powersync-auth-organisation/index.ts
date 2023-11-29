39be435f7b4f6a38dbca528a6188e60de23c0744
Gideon <gideonb@thedelta.io>
2023-11-29 13:36:00 +0200

    Added Staging DB info

--- supabase/functions/powersync-auth-organisation/index.ts
+++ supabase/functions/powersync-auth-organisation/index.ts
@@ -1,8 +1,8 @@
-import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
-import { getCorsHeaders } from "../_shared/cors.ts";
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import * as jose from "https://deno.land/x/jose@v4.14.4/index.ts";
-import * as base64 from "https://deno.land/std@0.196.0/encoding/base64.ts";
+import { Pool } from 'https://deno.land/x/postgres@v0.17.0/mod.ts';
+import { getCorsHeaders } from '../_shared/cors.ts';
+import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
+import * as jose from 'https://deno.land/x/jose@v4.14.4/index.ts';
+import * as base64 from 'https://deno.land/std@0.196.0/encoding/base64.ts';
 
 // The goal of this edge function is to provide an Auth API endpoint
 // The user will use a pin (i.e. password) to login using this Auth API endpoint
@@ -15,7 +15,7 @@ console.log(`Function "powersync-auth-organisation" up and running!`);
 
 const powerSyncPrivateKey = JSON.parse(
   new TextDecoder().decode(
-    base64.decode(Deno.env.get("POWERSYNC_PRIVATE_KEY")!)
+    base64.decode(Deno.env.get('POWERSYNC_PRIVATE_KEY')!)
   )
 ) as jose.JWK;
 
@@ -23,34 +23,35 @@ const powerSyncKey = (await jose.importJWK(
   powerSyncPrivateKey
 )) as jose.KeyLike;
 
-const powerSyncUrl = Deno.env.get("POWERSYNC_URL")!;
-const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
+const powerSyncUrl = Deno.env.get('POWERSYNC_URL')!;
+const supabaseUrl = Deno.env.get('SUPABASE_URL')!;
 
-// Connect to Database
+// Connect to Staging Database
 // Get the Database SSL certificate from AWS S3
-const response = await fetch(Deno.env.get("DATABASE_SSL_CERT_STORAGE_URL")!);
+const response = await fetch(Deno.env.get('DATABASE_SSL_CERT_STORAGE_URL')!);
 // The response is of type pem, to be used for the database connection pool
 const certificate = await response.text();
 
 const pool = new Pool(
   {
+    //Staging details
     tls: { caCertificates: [certificate] },
-    database: "postgres",
-    hostname: "workweek-prod.c4ffsv6b9ash.eu-west-1.rds.amazonaws.com",
-    user: "dev_admin",
+    database: 'postgres',
+    hostname: 'workweek-staging.c4ffsv6b9ash.eu-west-1.rds.amazonaws.com',
+    user: 'dev_admin',
     port: 5432,
-    password: Deno.env.get("DATABASE_PASSWORD")!,
+    password: Deno.env.get('DATABASE_PASSWORD')!,
   },
   3, // Max number of connections
   true // Enable lazy loading
 );
 
 serve(async (req: Request) => {
-  const origin = req.headers.get("Origin");
+  const origin = req.headers.get('Origin');
   const corsHeaders = getCorsHeaders(origin!);
 
-  if (req.method === "OPTIONS") {
-    return new Response("ok", { headers: corsHeaders });
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders });
   }
   try {
     const { organisationPin } = await req.json(); // Get the organisationPin from the request body
@@ -59,7 +60,7 @@ serve(async (req: Request) => {
     if (!organisationPin || organisationPin.length !== 5) {
       return new Response(
         JSON.stringify({
-          message: "organisationPin is required and must be 5 chars long",
+          message: 'organisationPin is required and must be 5 chars long',
         }),
         { status: 400, headers: corsHeaders }
       );
@@ -84,7 +85,7 @@ serve(async (req: Request) => {
       if (organisation.deleted_at) {
         return new Response(
           JSON.stringify({
-            message: "organisation was deleted",
+            message: 'organisation was deleted',
           }),
           { status: 404, headers: corsHeaders }
         );
@@ -94,7 +95,7 @@ serve(async (req: Request) => {
       if (!organisation) {
         return new Response(
           JSON.stringify({
-            message: "organisation not found",
+            message: 'organisation not found',
           }),
           { status: 404, headers: corsHeaders }
         );
@@ -113,7 +114,7 @@ serve(async (req: Request) => {
         .setIssuedAt()
         .setIssuer(supabaseUrl)
         .setAudience(powerSyncUrl)
-        .setExpirationTime("5m")
+        .setExpirationTime('5m')
         .sign(powerSyncKey);
       return new Response(
         JSON.stringify({
@@ -121,7 +122,7 @@ serve(async (req: Request) => {
           powersync_url: powerSyncUrl,
         }),
         {
-          headers: { "Content-Type": "application/json" },
+          headers: { 'Content-Type': 'application/json' },
           status: 200,
         }
       );
@@ -131,7 +132,7 @@ serve(async (req: Request) => {
     }
   } catch (error) {
     return new Response(JSON.stringify({ error: error.message }), {
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
       status: 500,
     });
   }
